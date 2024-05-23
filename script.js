b44eb91b14b2a3f0187994282edf92e2ebe1dd9d
Rylan Maxwell <Rylan.Maxwell@stu.ocsb.ca>
2024-05-23 08:46:10 -0400

    .

--- script.js
+++ script.js
@@ -9,4 +9,26 @@
 
 'use strict';
 
+document.getElementById("add_click_btn").addEventListener("click", increment)
+document.getElementById("sub_click_btn").addEventListener("click", subtract)
+document.getElementById("reset_click_btn").addEventListener("click", reset)
 
+let counter = 0;
+
+console.log(document.title);
+
+function increment(){
+    counter++
+    document.getElementById("click_count").innerText=counter;
+    return counter
+}
+function subtract(){
+    counter--
+    document.getElementById("click_count").innerText=counter;
+    return counter
+}
+function reset(){
+    counter=0
+    document.getElementById("click_count").innerText=counter;
+    return counter
+}
